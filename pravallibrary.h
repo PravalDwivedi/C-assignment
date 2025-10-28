8e0d6cd11e05cef8ddb9dc9e860a67c46cfeb2ad
PravalDwivedi <neeshudubey54@gmail.com>
2025-10-28 11:18:44 +0530

    pravallibrary.h

---
+++ pravallibrary.h
@@ -0,0 +1,10 @@
+
+#ifndef PRAVAL_H
+#define PRAVAL_H
+// Number functions
+int reverseDigits(int n);
+int isArmstrong(int num);
+int isAdams(int num);
+int isPrime(int num);
+int isPrimePalindrome(int num);
+
